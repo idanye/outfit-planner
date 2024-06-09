c117e4bb9cbf869d66832a986b919bab4b70592d
Nofar nahum <nofarnahum2@gmail.com>
2024-06-09 22:30:37 +0300

    result images saves in resultImages and have a uniq name

--- backend/testing_models.py
+++ backend/testing_models.py
@@ -5,35 +5,41 @@ from io import BytesIO
 import shutil
 import os
 
+
+def move_image(original_img_path, counter):
+    filename_without_extension, extension = os.path.splitext(os.path.basename(original_img_path))
+    # filename = os.path.basename(original_img_path)
+    new_filename = f"{filename_without_extension}_{counter}{extension}"
+    # Define the new path including the filename
+    destination_path = os.path.join("C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\backend\\resultImages", new_filename)
+    # Rename (move) the file
+    # os.rename(original_img_path, destination_path)
+    
+    # Use shutil.move to overwrite the file if it already exists
+    shutil.move(original_img_path, destination_path)
+
 # Local file paths
-model_image_path = "C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\modelsImages\\model_image2.png"
-garment_image_path = "C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\garmentsImages\\garment_image.jpeg"
+model_image_path = "C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\backend\\modelsImages\\model4.png"
+garment_image_path = "C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\backend\\\garmentsImages\\garment7.jpg"
 
 # Initialize the Gradio client
 client = Client("https://levihsu-ootdiffusion.hf.space/--replicas/iif7h/")
 
-# Make the API call
-result = client.predict(
-    file(model_image_path),  # Use gradio_client.file() to specify the file path
-    file(garment_image_path),  # Use gradio_client.file() to specify the file path
-    "Lower-body",  # Literal['Upper-body', 'Lower-body', 'Dress']  in 'Garment category (important option!!!)'
-    # Dropdown component
-    1,  # float (numeric value between 1 and 4) in 'Images' Slider component
-    20,  # float (numeric value between 20 and 40) in 'Steps' Slider component
-    1,  # float (numeric value between 1.0 and 5.0) in 'Guidance scale' Slider component
-    -1,  # float (numeric value between -1 and 2147483647) in 'Seed' Slider component
-    api_name="/process_dc"
-)
+for i in range(1,3):
+    # Make the API call
+    result = client.predict(
+        file(model_image_path),  # Use gradio_client.file() to specify the file path
+        file(garment_image_path),  # Use gradio_client.file() to specify the file path
+        "Lower-body",  # Literal['Upper-body', 'Lower-body', 'Dress']  in 'Garment category (important option!!!)'
+        # Dropdown component
+        1,  # float (numeric value between 1 and 4) in 'Images' Slider component
+        20,  # float (numeric value between 20 and 40) in 'Steps' Slider component
+        1,  # float (numeric value between 1.0 and 5.0) in 'Guidance scale' Slider component
+        -1,  # float (numeric value between -1 and 2147483647) in 'Seed' Slider component
+        api_name="/process_dc"
+    )
 
-# Print the result
-print(result)
-
-# # Assuming the result is a file path
-# result_image_path = result['output'][0]  # Adjust this based on the actual output structure
-
-
-# # Define the new path for the result image
-# new_result_image_path = os.path.join("C:\\Users\\Nofar\\year4SemB\\FromIdeaToApp\\outfit-planner\\outputImages", os.path.basename(result_image_path))
-
-# # Move the result image to the desired directory
-# shutil.move(result_image_path, new_result_image_path)
+    # print(result)
+    original_img_path = result[0]['image']
+    # extracted_path = result[0]['image'].replace("\\\\", "\\")
+    move_image(original_img_path, i)
